3812868975d17e838c381b2bc39d6fd25d2a556a
Abdullah02-Sec <abdullah.alheyari@optimumpartners.co>
2024-12-31 14:50:21 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Hello Morad");
+const http = require('http');
+
+
+const server = http.createServer((req, res) => {
+  res.end('Hello Morad!');
+});
+
+
+server.listen(3000, () => { console.log(`Server is running on port 3000!`);
+});
